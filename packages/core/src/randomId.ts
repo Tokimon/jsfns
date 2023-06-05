4e2b89fd673d3d2cbd231046e53f1ca6ef3e3835
Tokimon <tvo@leapwork.com>
2023-06-05 17:44:33 +0200

    randomId - avoid use of .substr

--- packages/core/src/randomId.ts
+++ packages/core/src/randomId.ts
@@ -11,6 +11,6 @@
  */
 export default function randomId(length = 10): string {
   let id = '';
-  while (id.length < length) { id += Math.random().toString(36).substr(2); }
-  return id.substr(0, length);
+  while (id.length < length) { id += Math.random().toString(36).slice(2); }
+  return id.slice(0, length);
 }
