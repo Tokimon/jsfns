a440347a0d6183a207c11048c192c4aba9656972
Tokimon <tvo@leapwork.com>
2023-06-06 12:43:09 +0200

    Try and avoid the double markdown init

--- docs/src/components/markdown.svelte
+++ docs/src/components/markdown.svelte
@@ -1,17 +1,6 @@
 <script context="module" lang="ts">
-  import hljs from 'highlight.js';
-  import { marked } from 'marked';
-  import { markedHighlight } from "marked-highlight";
-  
-  import "highlight.js/styles/github-dark.css";
-
-  marked.use(markedHighlight({
-    langPrefix: 'hljs language-',
-    highlight(code, lang) {
-      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-      return hljs.highlight(code, { language }).value;
-    }
-  }));
+  import prepareMarkdown from '../utils/prepareMarkdown';
+  const marked = prepareMarkdown();
 </script>
 
 <script lang="ts">
