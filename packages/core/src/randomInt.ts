11937e424a780e19720d6473b81cafc676cc70f9
Tokimon <tvo@leapwork.com>
2023-06-06 12:52:27 +0200

    randomInt: Improve type descriptions

--- packages/core/src/randomInt.ts
+++ packages/core/src/randomInt.ts
@@ -1,34 +1,48 @@
 const random = (num: number): number => Math.round(Math.random() * num);
 
 const range = (num: number, num2: number): number => {
-  if (num2 < num) {
-    [num, num2] = [num2, num];
-  }
-
+  if (num2 < num) [num, num2] = [num2, num];
   return random(num2 - num) + num;
 };
-
 
 
 /**
  * Returns a random integer from a base number or range of numbers
  *
- * @param num - The base number to generate the random number from
- * @param num2 - The second number in a range to create the number between
+ * @param min - The minimum number to generate
+ * @param max - The maximum number to generate
  *
- * @returns The generated random number
+ * @returns A random number between the given min and max
  *
  * @example
  * ```ts
- * // Direct base number
- * randomInt(100); // -> a number between 0 and 100
- *
- * // A range of numbers
  * randomInt(100, 200); // -> a number between 100 and 200
  * ```
  */
-export default function randomInt(num: number, num2?: number): number {
+function randomInt(min: number, max: number): number;
+
+/**
+ * Returns a random integer from a base number or range of numbers
+ *
+ * @param max - The maximum number to generate
+ *
+ * @returns A random number between 0 and the given max
+ *
+ * @example
+ * ```ts
+ * // Any random number
+ * randomInt(); // -> a number between 0 and Number.MAX_SAFE_INTEGER
+ * 
+ * // With max number
+ * randomInt(100); // -> a number between 0 and 100
+ * ```
+ */
+function randomInt(max?: number): number;
+
+function randomInt(num = Number.MAX_SAFE_INTEGER, num2?: number): number {
   return num2 === undefined
     ? random(num)
     : range(num, num2);
 }
+
+export default randomInt
