956ee55d235bed112cc079fa8acaa75da02a801a
Tokimon <tvo@leapwork.com>
2023-06-07 13:06:20 +0200

    Correct currencyFormat

--- packages/core/src/currencyFormat.ts
+++ packages/core/src/currencyFormat.ts
@@ -1,8 +1,8 @@
-import formatNumber from './formatNumber';
+import formatNumber, { type FormatNumberSettings } from './formatNumber';
 
 
 
-export type CurrencyFomatter = (num: number) => string;
+export type CurrencyFormatter = (num: number) => string;
 
 
 
@@ -17,7 +17,7 @@ const thousandRegExp = /^(\D*)1(\D*)000(\D*)(\d*)(\D*)$/;
  * symbols (no numbers), a thousand separator and a decimal separator followed by
  * the number of decimals defined with zeroes: `[before]1[thou.]000[dec.]00[after] -> $ 1,000.00`
  *
- * @param thousand - The template for how to format a number, takes an example of 1000 in the desired currency (eg. '1.000,00 €')
+ * @param thousandTemplate - The template for how to format a number, takes an example of 1000 in the desired currency (eg. '1.000,00 €')
  *
  * @returns Curried function to format a given number
  *
@@ -41,14 +41,14 @@ const thousandRegExp = /^(\D*)1(\D*)000(\D*)(\d*)(\D*)$/;
  * sixDecimals(234.12345678); // -> '$ 234.123457'
  * ```
  */
-export function currencyFormat(thousandString = '1.000,00 €'): CurrencyFomatter {
-  let m = thousandRegExp.exec(thousandString);
+export function currencyFormat(thousandTemplate = '1.000,00 €'): CurrencyFormatter {
+  let m = thousandRegExp.exec(thousandTemplate);
 
   if (!m) m = thousandRegExp.exec('1.000,00 €') as RegExpExecArray;
 
-  const [before, thousand, decimal, dec, after] = m.slice(1);
+  const [before, thousand, decimal, decimals, after] = m.slice(1);
 
-  const settings = { decimals: dec.length, thousand, decimal };
+  const settings: FormatNumberSettings = { decimalCount: decimals.length, thousand, decimal };
 
   return (num: number) => `${before}${formatNumber(num, settings)}${after}`;
 }
