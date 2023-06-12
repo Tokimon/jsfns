6e79ab56269c91c7538682890207ae00709c4ed3
Tokimon <tvo@leapwork.com>
2023-06-12 10:53:10 +0200

    rename hasClass to hasClassName (and add a hasAllClassNames function)

---
+++ packages/web/src/hasClassName.ts
@@ -0,0 +1,62 @@
+/**
+ * Does all (or any) of the listed class names exist in the DOM elements list of class names
+ *
+ * @param elm - DOM element to test
+ * @param classNames - Class names to test
+ * @param any - Test if at least one class name exist
+ * @returns All/any class names listed were found in the elements list of class names
+ *
+ * @example
+ *
+ * ```ts
+ * hasClass(myElm, 'open') // --> true if myElm has the "open" class name
+ * hasClass(myElm, ['open', 'shown']) // -> true if myElm has some or all given class names
+ * hasClass(myElm, ['open', 'shown'], true) // -> true if myElm has all given class names
+ * ```
+ */
+function hasClassName(elm: Element, classNames: string | string[], any?: boolean): boolean {
+  const checkFn = any ? 'some' : 'every';
+  const cns = !Array.isArray(classNames) ? [classNames] : classNames;
+
+  return cns[checkFn]((cn) => elm.classList.contains(cn));
+}
+
+export default hasClassName;
+
+
+
+/**
+ * Does any of the listed class names exist in the DOM elements list of class names
+ *
+ * @param elm - DOM element to test
+ * @param classNames - Class names to test
+ * @returns At least one of the class names listed were found in the elements list of class names
+ *
+ * @example
+ *
+ * ```ts
+ * hasAnyClassName(myElm, 'open') // --> true if myElm has the "open" class name
+ * hasAnyClassName(myElm, ['open', 'shown']) // -> true if myElm has at least one of the given class names
+ * ```
+ */
+export function hasAnyClassName(elm: Element, classNames: string | string[]): boolean {
+  return hasClassName(elm, classNames, true);
+}
+
+/**
+ * Does all of the listed class names exist in the DOM elements list of class names
+ *
+ * @param elm - DOM element to test
+ * @param classNames - Class names to test
+ * @returns All of the class names listed were found in the elements list of class names
+ *
+ * @example
+ *
+ * ```ts
+ * hasAllClassNames(myElm, 'open') // --> true if myElm has the "open" class name
+ * hasAllClassNames(myElm, ['open', 'shown']) // -> true if myElm has all given class names
+ * ```
+ */
+export function hasAllClassNames(elm: Element, classNames: string | string[]): boolean {
+  return hasClassName(elm, classNames, false);
+}
---
+++ packages/web/tests/hasClassName.test.ts
@@ -0,0 +1,69 @@
+import { byId, generateId, insertHtml, removeElement } from './assets/helpers';
+
+import hasClassName, { hasAnyClassName, hasAllClassNames } from '~web/hasClassName';
+
+
+
+const testID = generateId('HasClass');
+
+
+
+describe('"hasClassName"', () => {
+  let testNode: HTMLElement;
+
+  beforeAll(() => {
+    insertHtml(`<div id="${testID}"></div>`);
+    testNode = byId(testID);
+  });
+
+  beforeEach(() => { testNode.className = 'class1 class2'; });
+
+  afterAll(() => removeElement(testID));
+
+  describe('"hasClassName"', () => {
+    it('Detects whether a DOM element has a given class name or not', () => {
+      expect(hasClassName(testNode, 'class1')).toBe(true);
+      expect(hasClassName(testNode, 'class3')).toBe(false);
+    });
+
+    describe('class names as Array', () => {
+      it('Detects when a DOM element has all of the given class names', () => {
+        expect(hasClassName(testNode, ['class1', 'class2'])).toBe(true);
+        expect(hasClassName(testNode, ['class1', 'class3'])).toBe(false);
+        expect(hasClassName(testNode, ['class3', 'class4'])).toBe(false);
+      });
+
+      it('Detect when a DOM element has any of the given class names (any = true)', () => {
+        expect(hasClassName(testNode, ['class1', 'class2'], true)).toBe(true);
+        expect(hasClassName(testNode, ['class1', 'class3'], true)).toBe(true);
+        expect(hasClassName(testNode, ['class3', 'class4'], true)).toBe(false);
+      });
+    });
+  });
+
+  describe('"hasAnyClass"', () => {
+    it('Detects whether a DOM element has a given class name or not', () => {
+      expect(hasAnyClassName(testNode, 'class1')).toBe(true);
+      expect(hasAnyClassName(testNode, 'class3')).toBe(false);
+    });
+
+    it('Detects when a DOM element has any of the given class names', () => {
+      expect(hasAnyClassName(testNode, ['class1', 'class2'])).toBe(true);
+      expect(hasAnyClassName(testNode, ['class1', 'class3'])).toBe(true);
+      expect(hasAnyClassName(testNode, ['class3', 'class4'])).toBe(false);
+    });
+  });
+
+  describe('"hasAllClassNames"', () => {
+    it('Detects whether a DOM element has a given class name or not', () => {
+      expect(hasAllClassNames(testNode, 'class1')).toBe(true);
+      expect(hasAllClassNames(testNode, 'class3')).toBe(false);
+    });
+
+    it('Detects when a DOM element has all of the given class names', () => {
+      expect(hasAllClassNames(testNode, ['class1', 'class2'])).toBe(true);
+      expect(hasAllClassNames(testNode, ['class1', 'class3'])).toBe(false);
+      expect(hasAllClassNames(testNode, ['class3', 'class4'])).toBe(false);
+    });
+  });
+});
