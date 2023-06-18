5d5bae039addc8d549641168e10edf6a7ed06266
Tokimon <tvo@leapwork.com>
2023-06-18 23:54:44 +0200

    Update toDOM

--- packages/web/src/toDOM.ts
+++ packages/web/src/toDOM.ts
@@ -4,7 +4,7 @@
  * @param html - HTML string to transform into nodes
  * @returns DOM elements that the HTML represented
  *
- * @remarks
+ * @remark
  *
  * To keep the method simple and short, this method uses the `<template />`
  * element to convert the HTML. For older browsers, either
@@ -14,18 +14,16 @@
  * @example
  *
  * ```ts
- * // HTML is returned as is, as it cannot be parsed easily
- * toDOM('<html><body><div /></body></html>');
+ * // HTML cannot be parsed easily
+ * toDOM('<html><body><div /></body></html>'); // -> null
  *
  * // Convert a given HTML string to HTML
  * toDOM('<div><a>link</a></div>')
  * // -> <div><a>link</a></div>
  * ```
  */
-export default function toDOM(html: string): string | HTMLCollection {
-  if (/<(html|body|head|frame(set)?)\b/.test(html)) {
-    return html;
-  }
+export default function toDOM(html: string): HTMLCollection | null {
+  if (/<(html|body|head|frame(set)?)\b/.test(html)) return null;
 
   const template = document.createElement('template');
   template.innerHTML = html;
