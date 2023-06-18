bf80fa47b51302709fe0986d80cd070a61194de7
Tokimon <tvo@leapwork.com>
2023-06-18 23:55:53 +0200

    Update wrap (Now takes a element as well as wrapping element)

--- packages/web/src/wrap.ts
+++ packages/web/src/wrap.ts
@@ -1,15 +1,32 @@
 import insertAfter from './insertAfter';
 
-function findEmptyElm(elm?: Element | null): Element | null | undefined {
+function findEmptyElm(elm: Element): Element {
   const child = elm?.firstElementChild;
   return child ? findEmptyElm(child) : elm;
 }
 
-export default (elm: Element, html: string) => {
-  if (html) {
-    const wrapElm = findEmptyElm(insertAfter(elm, html));
-    wrapElm?.appendChild(elm);
-  }
+/**
+ * Wrap a given element in the given HTML, selector or element
+ *
+ * @param elm - DOM element to wrap with the given HTML
+ * @param wrapElm - The HTML, selector or element to wrap the given element with
+ * @return If the wrapping was successful or not
+ *
+ * @example
+ *
+ * ```ts
+ * wrap(document.documentElement, '<div />') // --> false - you cannot wrap the <html> element
+ *
+ * wrap(MyElm, document.createElement('div')) // --> true
+ * wrap(MyElm, '<div class="wrap-element"><span><b /></span></div>') // --> true - element inserted into the <b> tag
+ * wrap(MyElm, '.wrap-element') // --> true
+ * ```
+ */
+export default (elm: Element, wrapElm: Element | string) => {
+  const wrapDom = insertAfter(elm, wrapElm);
+  if (!wrapDom) return false;
 
-  return elm;
+  findEmptyElm(wrapDom).appendChild(elm);
+
+  return true;
 };
