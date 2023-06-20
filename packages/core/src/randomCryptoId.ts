e713ad8e9220980d3e0ef5c651a68551e2b7c58a
Tokimon <tvo@leapwork.com>
2023-06-20 15:11:13 +0200

    Add randomCryptoId

---
+++ packages/core/src/randomCryptoId.ts
@@ -0,0 +1,30 @@
+/**
+ * Generate a random id of designated length - using cryptographic secure random numbers
+ *
+ * @remark
+ *
+ * This method differs from `crypto.randomUUID()` in the way that it can be of any specified length
+ * and doesn't contain any "-".
+ * The methods would be slightly slower than `randomId` since it is using `crypto.getRandomValues()`,
+ * but it is a more secure method.
+ *
+ * @param length - The desired length of the id (minimum 2)
+ * @returns A random generated id of a specified length
+ *
+ * @example
+ *
+ * ```ts
+ * randomCryptoId(); // -> eg. 'efuc6f1n4xf'
+ * randomCryptoId(20); // -> eg. '3vsmrbxlh9at0vhcsf1xh'
+ * ```
+ */
+export function randomCryptoId(length = 10): string {
+  length = Math.max(2, length);
+
+  const UiArr = new Uint32Array(Math.ceil(Math.max(1, length / 10)));
+  const numbers = globalThis.crypto.getRandomValues(UiArr);
+
+  return numbers.reduce((str, n) => str + n.toString(36), '').slice(0, length);
+}
+
+export default randomCryptoId;
