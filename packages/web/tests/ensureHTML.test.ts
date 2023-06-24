d54fe3f720124230137eb5b09011014e691c455f
Tokimon <tvo@leapwork.com>
2023-06-25 00:45:56 +0200

    update ensureHTML test

--- packages/web/tests/ensureHTML.test.ts
+++ packages/web/tests/ensureHTML.test.ts
@@ -6,6 +6,6 @@ describe('"ensureHTML"', () => {
   });
 
   it('Returns generated HTML from a selector', () => {
-    expect(ensureHTML('#id.class[name=test]')).toBe('<div id="id" name="test" class="class"></div>');
+    expect(ensureHTML('#id.class[name=test]')).toBe('<div name="test" id="id" class="class"></div>');
   });
 });
