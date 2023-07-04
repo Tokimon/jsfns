6b9f3b3e26aa398b2d6ab79d97497900bf66c18e
Tokimon <tvo@leapwork.com>
2023-07-05 00:09:03 +0200

    Add isHTMLChildElement

---
+++ packages/web/src/isHTMLChildElement.ts
@@ -0,0 +1,27 @@
+import { isHTMLElement } from './isHTMLElement';
+
+export interface HTMLChildElement extends HTMLElement {
+  offsetParent: NonNullable<HTMLElement['offsetParent']>;
+  parentElement: NonNullable<HTMLElement['parentElement']>;
+  parentNode: NonNullable<HTMLElement['parentNode']>;
+}
+
+/**
+ * Is the given object a DOM node child of a DOM element
+ *
+ * @param obj - The object to check
+ * @returns Is it a child HTMLElement or not
+ *
+ * @example
+ *
+ * ```ts
+ * isHTMLChildElement(document.body) // --> true
+ * isHTMLChildElement(document.getElementById('my-elm')) // --> true
+ * isHTMLChildElement(createDetachedDocument().body) // --> true
+ *
+ * isHTMLChildElement(document.documentElement) // --> false
+ * ```
+ */
+export const isHTMLChildElement = (node: unknown): node is HTMLChildElement => isHTMLElement(node) && node.offsetParent != null;
+
+export default isHTMLChildElement;
