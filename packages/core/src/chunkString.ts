2c72e6224d1b7294d3afa29859962b4f7e702f00
Tokimon <tvo@leapwork.com>
2023-07-06 23:41:57 +0200

    Improve chunkString

--- packages/core/src/chunkString.ts
+++ packages/core/src/chunkString.ts
@@ -2,16 +2,21 @@
  * Split a String up into smaller strings of a give size (eg. 'ABCDEF' -> [AB,CD,EF])
  *
  * @param str - String to split up
- * @param size - Size par chunk
+ * @param options.size - Size par chunk
+ * @param options.reverse - Should the chunks be taken in reverse order (['ab', 'cde'] vs ['abc', 'de'])
  *
  * @returns Array of string chunks
  *
  * @example
  * ```ts
  * chunkString('abcdefghijkl'); // --> ['ab', 'cd', 'ef', 'gh', 'ij', 'kl']
- * chunkString('abcdefghijkl', 4); // --> ['abcd', 'efgh', 'ijkl']
+ * chunkString('abcdefghij', { size: 4 }); // --> ['abcd', 'efgh', 'ij']
+ * chunkString('abcdefghij', { size: 4, reverse: true }); // --> ['ab', 'cdef', 'ghij']
  * ```
  */
-export const chunkString = (str: string, size = 2): string[] => `${str || ''}`.match(new RegExp(`.{1,${size}}(?=\\B|$)`, 'g')) || [];
+export function chunkString(str: string, { size = 2, reverse = false } = {}): string[] {
+  const rev = reverse ? `(?=(?:.{${size}})*$)` : '';
+  return str.match(new RegExp(`.{1,${size}}${rev}`, 'g')) || [];
+}
 
 export default chunkString;
