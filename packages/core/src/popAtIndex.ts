52676668016645fe4296d0b7a1e932a9d8c3fab6
Tokimon <tvo@leapwork.com>
2023-07-21 00:57:01 +0200

    Rename popIndex to popAtIndex

--- packages/core/src/popAtIndex.ts
+++ packages/core/src/popAtIndex.ts
@@ -1,6 +1,5 @@
 /**
  * Removes and returns an entry from a given array, at a designated index position.
- * WARNING: This is not a pure function and will alter the given array internally
  *
  * @param list - The Array to remove the item from
  * @param index - At what index to remove from
@@ -10,22 +9,23 @@
  *
  * @example
  * ```ts
- * popIndexPure([1,2,3], 1); // --> 2 (array will then be [1, 3])
+ * popIndexPure([1,2,3], 1); // --> [2,  [1, 3]]
  * ```
  */
-export function popIndexPure(list: unknown[], index: number): [unknown, unknown[]] {
+export function popAtIndexPure(list: unknown[], index: number): [unknown, unknown[]] {
   const value = list[index];
 
-  if (index >= 0 && index < list.length) {
-    list = list.slice(0, index).concat(list.slice(index + 1));
-  }
+  if (index >= 0 && index < list.length) list = list.slice(0, index).concat(list.slice(index + 1));
 
   return [value, list];
 }
 
 /**
  * Removes and returns an entry from a given array, at a designated index position.
- * WARNING: This is not a pure function and will alter the given array internally
+ *
+ * @remark
+ *
+ * This is not a pure function and will alter the given array internally
  *
  * @param list - The Array to remove the item from
  * @param index - At what index to remove from
@@ -35,10 +35,8 @@ export function popIndexPure(list: unknown[], index: number): [unknown, unknown[
  * popIndex([1,2,3], 1); // --> 2 (array will then be [1, 3])
  * ```
  */
-export function popIndex<T>(list: T[], index: number): T | undefined {
-  if (index >= 0 && index < list.length) {
-    return list.splice(index, 1)[0];
-  }
+export function popAtIndex<T>(list: T[], index: number): T | undefined {
+  if (index >= 0 && index < list.length) return list.splice(index, 1)[0];
 }
 
-export default popIndex;
+export default popAtIndex;
--- packages/core/tests/popAtIndex.test.ts
+++ packages/core/tests/popAtIndex.test.ts
@@ -1,44 +1,44 @@
-import { popIndex, popIndexPure } from '@jsfns/core/popIndex';
+import { popAtIndex, popAtIndexPure } from '@jsfns/core/popAtIndex';
 
-describe('"popIndex"', () => {
-  describe('"popIndex"', () => {
+describe('"popAtIndex"', () => {
+  describe('"popAtIndex"', () => {
     it('Returns the value a the given index', () => {
-      expect(popIndex([1, 2, 3], 1)).toBe(2);
+      expect(popAtIndex([1, 2, 3], 1)).toBe(2);
     });
 
     it('Removes the value a the given index from the given array', () => {
       const a = [1, 2, 3];
-      popIndex(a, 1);
+      popAtIndex(a, 1);
       expect(a).toEqual([1, 3]);
     });
 
     describe.each([-1, 10])('When index is out of bounds: %i', (index) => {
       it('Returns undefined', () => {
-        expect(popIndex([1, 2, 3], index)).toBe(undefined);
+        expect(popAtIndex([1, 2, 3], index)).toBe(undefined);
       });
 
       it('Does not alter the given array', () => {
         const a = [1, 2, 3];
-        popIndex(a, index);
+        popAtIndex(a, index);
         expect(a).toEqual(a);
       });
     });
   });
 
-  describe('"popIndexPure"', () => {
+  describe('"popAtIndexPure"', () => {
     it('Removes the value a the given index from the given array', () => {
-      expect(popIndexPure([1, 2, 3], 1)).toEqual([2, [1, 3]]);
+      expect(popAtIndexPure([1, 2, 3], 1)).toEqual([2, [1, 3]]);
     });
 
     describe.each([-1, 10])('When index is out of bounds: %i', (index) => {
       it('Returns undefined', () => {
         const a = [1, 2, 3];
-        expect(popIndexPure(a, index)).toEqual([undefined, a]);
+        expect(popAtIndexPure(a, index)).toEqual([undefined, a]);
       });
 
       it('Does not alter the given array', () => {
         const a = [1, 2, 3];
-        expect(popIndexPure(a, index)[1]).toBe(a);
+        expect(popAtIndexPure(a, index)[1]).toBe(a);
       });
     });
   });
