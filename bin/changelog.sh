cd6d363466f0aafde375494ff14af68631c4fa58
Toke Voltelen <tokevoltelen@gmail.com>
2025-05-11 02:34:34 +0200

    Add Changeset for versioning instead of lerna

---
+++ bin/changelog.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+changelog() {
+    PS3=$(printf "\e[33m@jsfns/\e[32m$1\e[0m semver version: ")
+    options=("major" "minor" "patch" "none")
+
+    select SELECTED in "${options[@]}"; do
+        if [[ "${SELECTED}" == "none" ]]; then
+            break
+        fi
+
+        pnpm changeset --empty
+        FILE=$(ls -t .changeset/*.md | head -n 1)
+
+        sed -i "2i\"@jsfns/$1\": $SELECTED" $FILE
+        echo "\nUpdates:\n-"
+
+        nano $FILE
+
+        break
+    done
+}
+
+changelog core
+changelog web
