612e2ea7c3fd373b6efdad3869bedcad9a888829
Toke Voltelen <tokevoltelen@gmail.com>
2026-03-13 19:27:19 +0100

    toWords: Add missing TSDoc description

--- packages/core/src/toWords.ts
+++ packages/core/src/toWords.ts
@@ -1,3 +1,20 @@
+/**
+ * Find the words in a string delimeterd by any non letter/number characters.
+ * Numbers will be a word in them selves, so something like `some99words` becomes `[some, 99, words]`.
+ *
+ * @param str - String to find words in
+ * @returns The words found in the string
+ *
+ * @example
+ *
+ * ```ts
+ * toWords('camelCase') // --> ['camel', 'Case']
+ * toWords('HTMLElement') // --> ['HTML', 'Element']
+ * toWords('kebab-case') // --> ['kebab', 'case']
+ * toWords('snake_case') // --> ['snake', 'case']
+ * toWords('some99words') // --> ['some', '99', 'words']
+ * ```
+ */
 export function toWords(str: string): string[] {
 	return str
 		.split(
