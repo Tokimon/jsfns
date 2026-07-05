5889ac1de802075ebdaa15750f6f0f529f9e367c
Toke Voltelen <tokevoltelen@gmail.com>
2026-07-05 18:10:49 +0200

    Core: Improve hexToNumber

--- packages/core/src/hexToNumber.ts
+++ packages/core/src/hexToNumber.ts
@@ -3,13 +3,17 @@
  *
  * @param hex - Hex code to parse
  *
- * @returns Numeric representation of the hex code
+ * @returns Numeric representation of the hex code or `NaN` if it could not be parsed correctly to a number
+ *
+ * @remarks Passing in numeric strings works and gives returns the number as parse with radix 16
  *
  * @example
  * ```ts
  * hexToNumber('ba'); // --> 186
+ * hexToNumber('gg'); // --> NaN
+ * hexToNumber('3000'); // --> 12288
  * ```
  */
-export const hexToNumber = (hex: string): number => (hex ? Number.parseInt(hex, 16) : 0);
+export const hexToNumber = (hex: string) => Number.parseInt(hex, 16);
 
 export default hexToNumber;
