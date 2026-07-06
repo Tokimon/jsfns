666d31e66c795c39d861b613e09042ae970ba8a3
Toke Voltelen <tokevoltelen@gmail.com>
2026-07-06 23:29:07 +0200

    Web: domReady rename to onDomReady and improve

--- packages/web/tests/onDomReady.test.ts
+++ packages/web/tests/onDomReady.test.ts
@@ -1,14 +1,14 @@
-import { docComplete, domReady } from '@jsfns/web/domReady.js';
+import { isDocComplete, onDomReady } from '@jsfns/web/onDomReady.js';
 import { describe, expect, it, vi } from 'vitest';
-import { bind, triggerEvent, unbind } from './assets/helpers.js';
+import { bind, triggerEvent, unbind } from './assets/helpers.ts';
 
 function afterDomLoad() {
 	return new Promise((resolve) => {
-		if (docComplete()) {
+		if (isDocComplete()) {
 			resolve('complete');
 		} else {
 			const onRdyState = () => {
-				if (docComplete()) {
+				if (isDocComplete()) {
 					resolve('complete');
 					unbind(document, 'readystatechange', onRdyState);
 				}
@@ -19,25 +19,25 @@ function afterDomLoad() {
 	});
 }
 
-describe('"domReady"', () => {
+describe('"onDomReady"', () => {
 	it('Triggers the handler on the document ready event', async () => {
 		await afterDomLoad();
 
-		const domReadyCb = vi.fn();
+		const onDomReadyCb = vi.fn();
 		const fakeReadyState = vi.spyOn(document, 'readyState', 'get').mockReturnValue('loading');
 
-		domReady(domReadyCb);
+		onDomReady(onDomReadyCb);
 		fakeReadyState.mockRestore();
 
 		triggerEvent('readystatechange');
 
-		expect(domReadyCb).toHaveBeenCalled();
+		expect(onDomReadyCb).toHaveBeenCalled();
 	});
 
 	it('Triggers the handler when the method is bound after the DOM has finished loading', async () => {
 		await afterDomLoad();
 		const cb = vi.fn();
-		domReady(cb);
+		onDomReady(cb);
 		expect(cb).toHaveBeenCalled();
 	});
 });
